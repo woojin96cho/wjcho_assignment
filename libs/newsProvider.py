ff12e9fdd491bce88bc06adf18be8f89dcea3d57
woojincho <woojincho@dgu.ac.kr>
2020-10-30 18:53:14 +0900

    2020-10-30 2주차 코드 첨부파일 구현, ITWorld라는 뉴스업체 추가, Pandas를 사용한 데이터 형태 정립

--- libs/newsProvider.py
+++ libs/newsProvider.py
@@ -80,7 +80,7 @@ def get_boanNews():
                 news_name = tmp_name.replace('<dt>', '').replace('</dt>', '')
                 news_addr = "https://m.boannews.com/html/" + tmp_addr.replace('<li>', '').replace('</li>', '').split('href="')[1].split('">')[0].replace('amp;','')
                 #print(news_name,news_addr)
-                news.append([news_name,news_addr])
+                news.append([news_name,news_addr, "보안뉴스"])
         #print(news)
 
 
@@ -108,6 +108,70 @@ def get_yonhapNews():
 
         news = []
         for i in range(len(data)):
-            news.append([data['TITLE'][i], "https://m.yna.co.kr/" + data['URL'][i]])
+            news.append([data['TITLE'][i], "https://m.yna.co.kr/" + data['URL'][i], "연합뉴스"])
 
         return news
+
+
+def get_itworld():
+    with requests.Session() as s:
+        before_auth_header = {
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Sec-Fetch-Site': 'none',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Dest': 'document',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'ko-KR,ko;q=0.9'
+        }
+
+        cookie = ''
+        # Get Cookie Value
+        tmp = s.get('http://www.itworld.co.kr/news?page=1', headers=before_auth_header, verify=False)
+        #print(tmp.headers)
+        #print(tmp.content)
+        cookie = tmp.headers['Set-Cookie']
+        #print(cookie)
+
+
+        cookie_include_header = {
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'ko-KR,ko;q=0.9',
+            'Cookie': cookie
+        }
+
+        '''
+              <h4 class="news_list_full_size news_list_title default_font font_bold font_limit title_h3" title="아이폰 12 프로 심층 리뷰 : 미래를 위한 아이폰">
+            <a href="/news/170164">아이폰 12 프로 심층 리뷰 : 미래를 위한 아이폰</a>
+            </h4>
+        '''
+        # bs4_test_body = '<h4 class="news_list_full_size news_list_title default_font font_bold font_limit title_h3" title="아이폰 12 프로 심층 리뷰 : 미래를 위한 아이폰"><a href="/news/170164">아이폰 12 프로 심층 리뷰 : 미래를 위한 아이폰</a></h4>'
+        # soap = BeautifulSoup(bs4_test_body, 'html.parser')
+        # names = soap.findAll('h4')
+        # # addr = soap.findAll('li')
+        # print(names)
+
+        news = []
+        for i in range(1, 11):
+            all_news_body  = 'tab_type=1&page1='+str(i)
+            news_need_parse = s.post('http://www.itworld.co.kr/news?page='+str(i), headers = cookie_include_header, data = all_news_body)
+            # print(news_need_parse.text)
+            soap = BeautifulSoup(news_need_parse.text, 'html.parser')
+            names = soap.findAll('h4')
+            for j in range(0, len(names)):
+                tmp_name = str(names[j])
+                tmp_addr = str(names[j]).split('<a href=')[1].split('">')[0]
+                news_name = tmp_name.replace('<h4 class="news_list_full_size news_list_title default_font font_bold font_limit title_h3" title="', '').split('">')[0].replace('&amp;', '&')
+                news_addr = "http://www.itworld.co.kr" + tmp_addr.replace('"','')
+                #print(news_name,news_addr)
+                news.append([news_name,news_addr,"ITWorld"])
+        #print(news)
+
+
+    return news
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import libs.newsProvider as provider
 import pandas as pd
+import datetime
+
+now = datetime.datetime.now()
 
 f  = open('./dictionary', 'r', encoding = 'utf-8-sig')
 dict = f.readlines()
@@ -7,16 +10,22 @@ dict = f.readlines()
 for i in range(len(dict)):
     dict[i] = dict[i].replace('\n', '')
 
-interest_news = []
+interest_news = pd.DataFrame(columns=["제목", "URL", "제공업체"])
 all_news=  []
+
 
 all_news.append(provider.get_yonhapNews())
 all_news.append(provider.get_boanNews())
+all_news.append(provider.get_itworld())
 
 for i in range(len(all_news)):
     for j in range(len(all_news[i])):
         for k in range(len(dict)):
             if dict[k] in all_news[i][j][0]:
-                interest_news.append([all_news[i][j][0], all_news[i][j][1]])
+                interest_news = interest_news.append({"제목":all_news[i][j][0], "URL":all_news[i][j][1], "제공업체":all_news[i][j][2]}, ignore_index=True)
 
+interest_news.index = interest_news.index + 1
 print(interest_news)
+attach_name = now.strftime('%y%m%d')+"_뉴스.xls"
+
+interest_news.to_excel('./MailAttachment/'+attach_name, encoding='utf-8')
