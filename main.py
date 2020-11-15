2b04d959647c04e7d9c9daa606eabaa64e461d02
woojincho <woojincho@dgu.ac.kr>
2020-11-15 13:41:42 +0900

    # 2020-11-15 2020-11-08 버전에서 Password가 들어간 소스가 업로드되어 git reset hard 진행하였습니다. 해당사항으로 인해 11-08데이터가 없는걸로 보여서 evidence.zip에 해당 사항 사진 업로드 하였습니다. smtplib 테스트 및 안정화.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import smtplib
 import libs.newsProvider as provider
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
 import pandas as pd
 import datetime
+
 
 now = datetime.datetime.now()
 
@@ -26,6 +31,41 @@ for i in range(len(all_news)):
 
 interest_news.index = interest_news.index + 1
 print(interest_news)
-attach_name = now.strftime('%y%m%d')+"_뉴스.xls"
+attach_name = now.strftime('%y%m%d')+"_News.xls"
 
 interest_news.to_excel('./MailAttachment/'+attach_name, encoding='utf-8')
+
+
+def sendMail(email, password ,df,receiver):
+    news_html = df.to_html()
+    msg_body = str(news_html)
+    messageHTML = msg_body
+    global attach_name
+
+    msg = MIMEMultipart('alternative')
+    msg['From'] = email
+    msg['To'] = receiver
+    msg['Subject'] = 'Interest News - ' + now.strftime('%y%m%d')
+    messagePlain = 'Interest News - ' + now.strftime('%y%m%d')
+    msg.attach(MIMEText(messagePlain, 'plain'))
+    msg.attach(MIMEText(messageHTML, 'html'))
+
+    fb  = open('./MailAttachment/'+attach_name, 'rb')
+    file = MIMEApplication(fb.read())
+    file['Content-Disposition'] = 'attachment; filename="'+attach_name+'"'
+    msg.attach(file)
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(email, password)
+    text = msg.as_string()
+    server.sendmail(email, receiver, text)
+    server.quit()
+    # try:
+
+
+    # except Exception as e:
+    #     print("Fail To Send Mail.(" + str(e)+")")
+
+
+
+sendMail("","", interest_news, "")
